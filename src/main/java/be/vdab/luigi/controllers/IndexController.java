08fbe8a3a5f80af11bd788dab21878e2780dc100
caglarcercinli <chaglar.ronin@gmail.com>
2021-03-18 17:34:46 +0100

    volgende dag

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,19 +1,20 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index(){
-        var morgenOfMiddag = LocalTime.now().getHour() <12 ? "morgen": "middag";
-        return "<!doctype html><html><html><title>Hallo</title>Goede "
-                +morgenOfMiddag+"</body></html>";
+    public ModelAndView index(){
+        var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        return new ModelAndView("index","moment",morgenOfMiddag);
     }
 
 }

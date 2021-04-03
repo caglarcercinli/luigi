26555147449c955d7f157eafe2325acdd0457719
caglarcercinli <chaglar.ronin@gmail.com>
2021-04-03 17:53:39 +0200

    week 3, dag 6

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -2,16 +2,19 @@ package be.vdab.luigi.controllers;
 
 import be.vdab.luigi.domain.Pizza;
 import be.vdab.luigi.exceptions.KoersClientException;
+import be.vdab.luigi.forms.VanTotPrijsForm;
 import be.vdab.luigi.services.EuroService;
 import be.vdab.luigi.services.PizzaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
@@ -84,5 +87,19 @@ public class PizzaController {
     public ModelAndView pizzasMetEenPrijs(@PathVariable BigDecimal prijs) {
         return new ModelAndView("prijzen", "pizzas", pizzaService.findByPrijs(prijs))
                 .addObject("prijzen", pizzaService.findUniekePrijzen());
+    }
+    @GetMapping("vantotprijs/form")
+    public ModelAndView vanTotPrijsForm(){
+        return new ModelAndView("vantotprijs")
+                .addObject(new VanTotPrijsForm(null,null));
+    }
+    @GetMapping("vantotprijs")
+    public ModelAndView vanTotPrijs(@Valid VanTotPrijsForm form, Errors errors){
+        var modelAndView = new ModelAndView("vantotprijs");
+        if(errors.hasErrors()){
+            return modelAndView;
+        }
+        return modelAndView.addObject("pizzas",
+                pizzaService.findByPrijsBetween(form.getVan(),form.getTot()));
     }
 }
